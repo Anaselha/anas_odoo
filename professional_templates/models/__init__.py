e1f9bc471f1b27f7ac1a5f64deaa961d0309f77c
Anaselha <101324373+Anaselha@users.noreply.github.com>
2024-07-11 09:13:58 +0100

    file upload

---
+++ professional_templates/models/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Odoo Proprietary License v1.0.
+
+see License:
+https://www.odoo.com/documentation/user/15.0/legal/licenses/licenses.html#odoo-apps
+# Copyright ©2022 Bernard K. Too<bernard.too@optima.co.ke>
+"""
+
+from . import ir_actions_report
+from . import account_journal
+from . import report
+from . import res_currency
+from . import report_settings
+from . import res_company
+from . import account_payment
+from . import res_config_settings
+from . import res_partner
+from . import invoice
+from . import sale_order
+from . import purchase_order
+from . import rfq
+from . import delivery_note
+from . import picking
